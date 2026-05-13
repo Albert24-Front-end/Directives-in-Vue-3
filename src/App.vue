5742170dc904e14fa13bbde014d917424ea41a72
Albert <albertaliev777@gmail.com>
2026-05-13 16:29:50 +0500

    simple way to create custom v-lazy directive for lazy image loading

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+const vLazy = {
+  mounted(el: HTMLImageElement, binding: { value: string }) {
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry?.isIntersecting) {
+        el.src = binding.value;
+        observer.unobserve(el);
+      }
+    })
+    observer.observe(el);
+  }
+}
+
+</script>
+
+<template>
+  <h1>Directives</h1>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <p>.</p>
+  <img width="900" v-lazy="'https://www.pixelstalk.net/wp-content/uploads/2025/08/Nature-wallpaper-for-desktop-aesthetic-an-enchanting-sunset-over-a-tranquil-beach-with-gentle-waves-lapping-at-the-shore.jpg'" />
+</template>
